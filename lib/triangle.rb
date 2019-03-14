de73ced0099eb3073640d523581b1da1a94abce5
Jonathan Wong <jonathan.wong110@gmail.com>
2019-03-14 20:58:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
     @side_3 = side_3
   end
   
-  def type()
+  def kind()
     if (@side_1 == @side_2) && (@side_2 == @side_3)
       :equilateral
     elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
