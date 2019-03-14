ea6e24b4e69c7ef2386d5d1c1add88a7fe2ab8b2
Jonathan Wong <jonathan.wong110@gmail.com>
2019-03-14 21:02:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,13 +12,12 @@ class Triangle
       raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
-    else
-      if (@side_1 == @side_2) && (@side_2 == @side_3)
-        :equilateral
-      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
-        :isosceles
-      else (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
-        :scalene
+    elsif (@side_1 == @side_2) && (@side_2 == @side_3)
+      :equilateral
+    elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+      :isosceles
+    else (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+      :scalene
       end
     end
   end
