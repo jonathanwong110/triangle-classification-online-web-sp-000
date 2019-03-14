c98890a8853c384fae0dda940bdcb5964ee3f229
Jonathan Wong <jonathan.wong110@gmail.com>
2019-03-14 20:54:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,7 +16,7 @@ class Triangle
         :equilateral
     elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
         :isosceles
-    elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+    else (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
         :scalene
       end
     end
